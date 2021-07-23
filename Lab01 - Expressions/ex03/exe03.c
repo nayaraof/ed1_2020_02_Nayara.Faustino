3ee788045894ae06a841151cce7e6872b3f0484d
nayaraof <80076803+nayaraof@users.noreply.github.com>
2021-07-22 21:15:10 -0300

    Update and rename ex03.c to exe03.c

--- Lab01 - Expressions/ex03/exe03.c
+++ Lab01 - Expressions/ex03/exe03.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
 int main() {
+    int x;
     printf("<< Sucessor e Antecessor >>\n");
+    printf("Digite um numero: ");
+    scanf("%d", &x);
+        printf("O antecessor de %d= %d,x,(x-1)):
+        printf("O sucessor de %d= %d,x,(x+1)):
+return 0 ;    
 }
 
 /*
