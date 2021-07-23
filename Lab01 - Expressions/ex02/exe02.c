4bff3b60f4a4f1241fd3605b6f02d5e0f2f4d00b
nayaraof <80076803+nayaraof@users.noreply.github.com>
2021-07-22 21:11:37 -0300

    Update and rename ex02.c to exe02.c

--- Lab01 - Expressions/ex02/exe02.c
+++ Lab01 - Expressions/ex02/exe02.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    printf("<< Conversor Temperatura >>\n");
+ float f,c;
+ printf("<< Conversor Temperatura >>\n");
+    
+    printf("Digite a temperatura em celsius: ");
+    scanf("%f",&c);
+     f = (c * (9.0/5.0)) + 32.0;
+    printf ("essa temperatura em celsius corresponde a seguinte temperatura em Em F:> %.2f\n", f);
+    system ("pause");
+    return 0;
 }
+    
 
 /*
 => Ler uma temperatura em graus Celsius e apresentá-la convertida em graus Fahrenheit. A fórmula de 
