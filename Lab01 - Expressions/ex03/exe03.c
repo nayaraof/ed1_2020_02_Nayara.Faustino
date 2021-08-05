5b2478b59c9be1a7d12b2aaf33c568409e2bdcd8
nayaraof <80076803+nayaraof@users.noreply.github.com>
2021-08-05 09:13:13 -0300

    Update exe03.c
    .

--- Lab01 - Expressions/ex03/exe03.c
+++ Lab01 - Expressions/ex03/exe03.c
@@ -10,17 +10,4 @@ int main() {
 return 0 ;    
 }
 
-/*
-=> Ler um número e retorne seu antecessor e seu sucessor.
 
--------------------------------------------------------------------------------
-Exemplo de saída:
-
-<< Sucessor e Antecessor >>
-Digite um numero: 2
-
-Antecessor: 1
-Numero: 2
-Sucessor: 3
--------------------------------------------------------------------------------
-*/
