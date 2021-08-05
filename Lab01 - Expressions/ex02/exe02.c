3aa8fa8150c98b82b208e7d36287c27dde5ffb1b
nayaraof <80076803+nayaraof@users.noreply.github.com>
2021-08-05 09:12:30 -0300

    Update exe02.c
    .

--- Lab01 - Expressions/ex02/exe02.c
+++ Lab01 - Expressions/ex02/exe02.c
@@ -13,15 +13,3 @@ int main() {
 }
     
 
-/*
-=> Ler uma temperatura em graus Celsius e apresentá-la convertida em graus Fahrenheit. A fórmula de 
-conversão é : F = C ∗ (9.0/5.0)+ 32.0, sendo F a temperatura em Fahrenheit e C a temperatura em Celsius.
-
--------------------------------------------------------------------------------
-Exemplo de saída:
-
-<< Conversor Temperatura >>
-Digite a temperatura (em Celsius): 2.5O 
-30 graus Celsius correspondem a 86 Fahrenheit
--------------------------------------------------------------------------------
-*/
