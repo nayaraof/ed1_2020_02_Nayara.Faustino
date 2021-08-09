5e78623b8795dc9274e21b997cd979847fa29295
nayaraof <80076803+nayaraof@users.noreply.github.com>
2021-08-09 19:56:03 -0300

    exe03

--- Lab05 - Arrays/ex03/ex03.c
+++ Lab05 - Arrays/ex03/ex03.c
@@ -1,22 +1,20 @@
-#include <stdio.h>
+int main()
+{
+    int val[6];
+    int i ;
+    printf("<< Listando um vetor em ordem inversa >>");
+    printf("\n");
+        for(i=0;i<6;i++){
+   printf("Entre com o numero %d :  ", i+1);
+   scanf("%d", &val[i]);
+                         }
+    printf("\n");
+    printf("A ordem inversa dos numeros eh: ");
+        for(i=5;i>=0;i--){
+    printf("%d", val[i]);
+                           }
 
-int main(){
-    printf("<< Listando um vetor em ordem inversa !!! >>\n");
-    return 0;
-}
+    return 0 ;
+    }
 
-/*
-Faça um programa em que o usuário digita 6 números inteiros e o programa mostra
-na tela os 6 números digitados na ordem inversa (obrigatório o uso de loops)
 
-Exemplo de saída:
-<< Listando um vetor em ordem inversa >>
-Entre com o numero 1: 5
-Entre com o numero 2: 6
-Entre com o numero 3: 4
-Entre com o numero 4: 0
-Entre com o numero 5: 3
-Entre com o numero 6: 6
-
-Ordem inversa dos numeros: 6 3 0 4 6 5
-*/
