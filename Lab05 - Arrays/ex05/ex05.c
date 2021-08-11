dc3759762d0249cf2e5edf54e6066e9e171c3400
nayaraof <80076803+nayaraof@users.noreply.github.com>
2021-08-11 14:53:14 -0300

    exe05

--- Lab05 - Arrays/ex05/ex05.c
+++ Lab05 - Arrays/ex05/ex05.c
@@ -1,26 +1,36 @@
-#include <stdio.h>
+{
+    int vet[5], i, maior, menor;
+    float media, soma;
 
-int main(){
-    printf("<< Five Values >>\n");
+    printf ("<< 5 valores >> \n");
+
+    for (i=0; i<5; i++){
+
+        printf ("Entre com o numero %d: ", i+1);
+        scanf ("%d",&vet[i]);
+        maior = vet[0];
+        menor = vet[0];
+
+    }
+    printf ("\n");
+    printf ("Os numeros digitados sao: ");
+
+     for(i=0; i<5; i++){
+
+        printf("%d ",vet[i]);
+
+        if(maior < vet[i])
+            maior = vet[i];
+        if(menor > vet[i])
+            menor = vet[i];
+
+        soma = soma + vet[i];
+        media = soma / 5;
+    }
+    printf("\n");
+    printf("O maior valor e: %d \n", maior);
+    printf("O menor valor e: %d \n", menor);
+    printf("A media eh: %.1f", media);
 
     return 0;
 }
-
-
-/*
-Fazer um programa para ler 5 valores, e, em seguida, mostrar todos os valores
-lidos juntamente com o maior (Max), o menor (Min) e a média (Mean) dos valores
-
-Exemplo de saída:
-<< Five Values >>
-Input #1: 5
-Input #2: 6
-Input #3: 4
-Input #4: 0
-Input #5: 3
-
-The numbers entered are: 5 6 4 0 3
-Max. Value: 6
-Min. Value: 0
-Mean: 3.6
-*/
