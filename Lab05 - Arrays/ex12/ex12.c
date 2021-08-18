53147dae8fa5faed34c483c2e4f317a11a8c0ee7
nayaraof <80076803+nayaraof@users.noreply.github.com>
2021-08-17 23:29:30 -0300

    exe12

--- Lab05 - Arrays/ex12/ex12.c
+++ Lab05 - Arrays/ex12/ex12.c
@@ -1,29 +1,57 @@
+12)
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 int main(){
-    printf("<< Probabilidades >>\n");
 
-    return 0;
+    int x=1;
+    int i=0;
+    float vA[3];
+    float vB[3];
+    float vC[3];
+
+    float verde = 0;
+    float azul = 0;
+    float amarela = 0;
+    float vermelha = 0;
+    float probverde = 0;
+    float probvermelha = 0;
+    float probazul = 0;
+    float probamarela = 0;
+    int total = 0;
+
+    printf("Digite a quantidade de bolinhas\n\n");
+
+     printf("\nVerde:");
+        fflush(stdin);
+        scanf("%f",&verde);
+
+       printf("\nAzul:");
+        fflush(stdin);
+        scanf("%f",&azul);
+
+        printf("\nAmarela:");
+        fflush(stdin);
+        scanf("%f",&amarela);
+
+        printf("\nVermelha:");
+        fflush(stdin);
+        scanf("%f",&vermelha);
+
+        total = verde + azul + amarela + vermelha;
+
+        printf("\n\nProbabilidades\n\n");
+
+        probverde = (verde / total) *100;
+        probazul = (azul / total) *100;
+        probamarela = (amarela / total) *100;
+        probvermelha = (vermelha / total) *100;
+
+        printf("Verde: %.2f\n", probverde);
+        printf("Azul: %.2f\n", probazul);
+        printf("Amarela: %.2f\n", probamarela);
+        printf("Vermelha: %.2f\n", probvermelha);
+
+   return 0;
 }
-
-/*
-Uma piscina de bolinhas possui bolas azuis, verdes, amarelas e vermelhas. 
-Faça um programa em que o usuário digita a quantidade de bolinhas de cada
-cor existente em uma piscina e mostra a probabilidade de ocorrência de cada 
-cor, indicando qual possui a maior probabilidade
-
-Exemplo de saída:
-<< Probabilidades >>
-
-Digite a quantidade de bolinhas
-Verde: 51
-Azul: 60
-Amarela: 70
-Vermelha: 80
-
-Probabilidades
-Verde: 19.5%
-Azul: 22.9%
-Amarela: 26.8%
-Vermelha: 30.6% << Maior probabilidade
-*/
