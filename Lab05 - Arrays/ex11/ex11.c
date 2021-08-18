d237b6eca1a4a94614c07697c75c3e12f4e686e4
nayaraof <80076803+nayaraof@users.noreply.github.com>
2021-08-17 23:28:28 -0300

    exe11

--- Lab05 - Arrays/ex11/ex11.c
+++ Lab05 - Arrays/ex11/ex11.c
@@ -1,29 +1,35 @@
-#include <stdio.h>
+ #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
-int main(){
-    printf("<< Subtracao de vetores >>\n");
+int main()
+{
+    int x=1;
+    int i=0;
+    int v1[5];
+    int v2[5];
+    int v[5];
 
-    return 0;
+    printf("Pares e Impares\n\n");
+
+    for(i=0;i<5;i++){
+        printf("Digite o valor %d",x);
+        scanf("%d", &v[i]);
+        x++;
+    }
+
+        x=0;
+
+     for(i=0;i<5;i++){
+        if(v[i]%2 == 0){
+                printf("Pares %d",v[i]);
+                x++;
+
+       } else
+            printf("Impares %d", v[i]);
+
+        }
+
+
+   return 0;
 }
-
-#include <stdio.h>
-
-int main(){
-    printf("<< Pares e Ímpares >>\n");
-
-    return 0;
-}
-
-/*
-Leia 5 números inteiros e armazene em um vetor v. Crie dois novos vetores v1 e v2. Copie os valores ímpares de v para v1, e os valores pares de v para v2. Note que cada um dos vetores v1 e v2 tem no máximo 5 elementos, mas nem todos os elementos são utilizados. No final escreva os elementos UTILIZADOS de v1 e v2.  
-Exemplo de saída:
-<< Pares e Ímpares >>
-Digite o valor 1: 51
-Digite o valor 2: 60
-Digite o valor 3: 70
-Digite o valor 4: 80
-Digite o valor 5: 60
-
-Ímpares: 51
-Pares: 60, 70, 80, 60
-*/
