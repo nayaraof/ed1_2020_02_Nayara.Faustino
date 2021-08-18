63c2103607f08370f6d8938c44b7cf63e6513580
nayaraof <80076803+nayaraof@users.noreply.github.com>
2021-08-18 15:22:34 -0300

    exe01

--- Lab09 - Structs/ex01/ex01.c
+++ Lab09 - Structs/ex01/ex01.c
@@ -1,32 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
-int main(){
-    printf("<<  >>\n");
+int main()
+{
 
-    return 0;
+    struct telefone{
+
+    int ddd;
+    int telefone;
+
+    };
+
+    int i = 0;
+    struct telefone telefone1[i];
+
+    for(i=0;i<3;i++){
+
+        printf("Digite o DDD: ");
+        fflush(stdin);
+        scanf("%d",&telefone1[i].ddd);
+
+        printf("Digite o Numero: ");
+        fflush(stdin);
+        scanf("%d",&telefone1[i].telefone);
+
+    }
+
+    for(i=0;i<3;i++){
+
+        printf("\nTelefone: %d %d",telefone1[i].ddd,telefone1[i].telefone);
+
+    }
+
+   return 0;
 }
-
-/*
-1) Crie uma estrutura chamada telefone, que armazena o DDD e o telefone (ilustração abaixo).  
-Cadastre 3 telefones e mostre na tela os telefones cadastrados. 
-
-Telefone**
-+------------+------------+
-| DDD        | Telefone   |  
-+------------+------------+
-
--------------------------------------------------------------------------------
-Exemplo de saída:
-Digite o DDD: 34
-Digite o telefone: 991501111
-Digite o DDD: 31
-Digite o telefone: 991502222
-Digite o DDD: 64
-Digite o telefone: 991503333
-
-Telefones cadastrados: 
-34 991501111
-31 991502222
-64 991503333
--------------------------------------------------------------------------------
-*/
