9e0cc0a8310dea0c9470f9581d155201d4100e2e
nayaraof <80076803+nayaraof@users.noreply.github.com>
2021-08-17 21:00:19 -0300

    exe09

--- Lab01 - Expressions/ex09/ex09.c
+++ Lab01 - Expressions/ex09/ex09.c
@@ -1,20 +1,13 @@
-#include <stdio.h>
-#include <math.h>
-
-int main() {
-    printf("<< Distancia >>\n");
-}
-
-/*
-=> Escreva um programa que leia as coordenadas x e y de pontos no R2 e calcule sua distância da origem (0,0).  
-Obs: faça #include <math.h> e use a função sqrt() para calcular a raiz de um número
-
--------------------------------------------------------------------------------
-Exemplo de saída:
-
-<< Distancia >>
-Entre com a coordenada x: 3
-Entre com a coordenada y: 4
-A distancia entre os pontos (3,4) e (0,0) eh 5.0
--------------------------------------------------------------------------------
-*/
+#include<stdio.h>
+#include<stdlib.h>
+	int main () {
+   int x;
+   int y;
+		float distancia;
+		printf(" digite a coordenada x:");
+		scanf("%d", &x);
+		printf("Digite a coordenada y:");
+		scanf("%d",&y);
+		printf("\n\nDistancia entre os pontos: %5.2f\n", distancia = sqrt(pow(x,2)+pow(y,2)));
+  return 0 ;
+  }
