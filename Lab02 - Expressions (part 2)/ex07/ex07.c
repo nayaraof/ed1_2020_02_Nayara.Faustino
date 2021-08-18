c73960de6357630114e770c1fc63fe363aed4a34
nayaraof <80076803+nayaraof@users.noreply.github.com>
2021-08-17 21:32:47 -0300

    exe07

--- Lab02 - Expressions (part 2)/ex07/ex07.c
+++ Lab02 - Expressions (part 2)/ex07/ex07.c
@@ -1,24 +1,36 @@
+ 
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
-int main() {
-    printf("== Operadores Booleanos ==\n");
+
+int main () {
+printf("<<Operadores Booleanos>>");
+printf("\n");
+
+int  num1,num2;
+printf("Entre com o primeiro valor :  ");
+scanf("%d",&num1);
+printf("\n");
+printf("Entre com o primeiro valor :  ");
+scanf("%d",&num2);
+printf("\n");
+
+    if ((num1 %2 == 0) && (num2  %2 == 0)  ){
+
+    printf(" 0 ");
+     }
+     else{
+    printf(" 1");
+     }
+   if ((num1 %2 == 0) || (num2  %2 == 0)  ){
+
+    printf(" 0 ");
+     }
+     else{
+    printf(" 1");
+     }
+     return 0 ;
+
 }
-
-/*
-=> Faça um algoritmo que mostre o funcionamento dos operadores lógicos. Peça para o usuário entrar com dois valores inteiros,, 
-onde 0 implica em FALSO e qualquer outro número implica em VERDADEIRO.
-Obs: XOR = (p || q) && !(p && q)
-
--------------------------------------------------------------------------------
-Exemplo de saída:
-
-== Operadores Booleanos ==
-Digite 0 para falso e nao 0 para verdadeiro
-Entre com o primeiro valor (A): -1
-Entre com o segundo valor (B): 3
-A and B: 1
-A or B: 1
-A xor B: 0
-not A: 0
--------------------------------------------------------------------------------
-*/
