cb4a4d8f14d792c7937247b0dd9d4c5c0038d6d6
nayaraof <80076803+nayaraof@users.noreply.github.com>
2021-08-18 19:56:00 -0300

    exe06

--- Lab10 - Pointers/ex06/ex06.c
+++ Lab10 - Pointers/ex06/ex06.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-    printf("<< Pointers >>\n");
+int main()
+{
+    void *num, *num1;
+    int p1, p2;
+    num = &p1;
+    num1 = &p2;
 
-    return 0;
+    printf ("Entre com dois valores: ");
+    scanf ("%i %lf", num, num1);
+
+    printf ("Os numeros sao: %d e %lf", *(int*)num, *(double*)num1);
+
 }
