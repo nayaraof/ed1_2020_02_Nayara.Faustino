54779e401aa25dee4fa5aa5627d0405dd97351b3
nayaraof <80076803+nayaraof@users.noreply.github.com>
2021-08-18 20:05:51 -0300

    exe03

--- Lab11 - Dynamic allocation/ex03/ex03.c
+++ Lab11 - Dynamic allocation/ex03/ex03.c
@@ -1,28 +1,29 @@
 #include <stdio.h>
-//lembre-se de incluir as bibliotecas adequadas
+#include <stdlib.h>
 
-int main(){
-    printf("<< Par ou Impar >>\n");
+int main()
+{
+    int *par_impar;
+    int num= 0, qntpar = 0;
+    int i = 0;
+    printf("Entre com a quantidade de inteiros: ");
+    scanf("%d", &num);
+    printf("\n");
+    par_impar = malloc(sizeof(int) * num);
+    for(i=0;i<num;i++)
+    {
+        printf("Digite o numero: ");
+        scanf("%d", &par_impar[i]);
+        printf("\n");
+    }
 
+    i = 0;
+    for(i=0;i<num;i++)
+    {
+        if(par_impar[i] % 2 == 0 )
+            qntpar++;
+    }
+    printf("Quantidade de pares: %d\n", qntpar);
+    printf("Quantidade de impares: %d\n", num - qntpar);
     return 0;
 }
-
-/*
-3) Faça um programa que leia n inteiros (definidos pelo usuário) 
-armazenando-os em uma memória alocada dinamicamente. Em seguida, mostre
-quantos dos n números são pares e quantos são ímpares.
-
-
-Exemplo de saída:
-<< Par ou Impar >>
-Quantos inteiros serao lidos: 5
-1º inteiro: 1
-2º inteiro: 2
-3º inteiro: 3
-4º inteiro: 4
-5º inteiro: 5
-
-Sao pares: 2 dos 5 inteiros lidos.
-Sao impares: 3 dos 5 inteiros lidos.
-
-*/
