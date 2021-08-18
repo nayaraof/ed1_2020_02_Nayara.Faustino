4de3fd597213a8eea0a6bdf375781b39654fc2ce
nayaraof <80076803+nayaraof@users.noreply.github.com>
2021-08-18 19:58:25 -0300

    exe01

--- Lab11 - Dynamic allocation/ex01/ex01.c
+++ Lab11 - Dynamic allocation/ex01/ex01.c
@@ -1,60 +1,23 @@
 #include <stdio.h>
-//lembre-se de incluir as bibliotecas adequadas
-
-int main(){
-    printf("<<  >>\n");
-
-    return 0;
-}
-
-/*
-1) Alocação dinâmica: outra utilidade dos ponteiros é que eles permitem 
-fazermos o que é chamado de alocação dinâmica. Isso significa que podemos 
-reservar espaços de memória enquanto estamos executando o programa. Seria
-como criar variáveis com o programa executando. Por exemplo, considere um
-vetor que armazenará o preço de produtos. Não sabemos quantos produtos serão
-cadastradas enquanto estamos programando. Podemos fazer alocação dinâmica 
-para resolver este problema. Implemente o programa abaixo, teste para diferentes
-valores de n e discuta o que significa o valor de n e de sizeof(double) no 
-commando malloc(n*sizeof(double)).
-
-
----------------------------------------------------------------
-#include <stdio.h>
 #include <stdlib.h>
 
-
-int main(void){
- double *produtos;
- int n,i;
-
- printf("Informe o número de produtos: ");
-  scanf("%d",&n);
-  // é necessário usar o comando malloc para alocar a memória
-  produtos = (double *)malloc(n*sizeof(double));
-
-  for   (i = 0; i < n; i++){
-    printf("Informe o valor do produto %d R$: ",i+1);
+int main()
+{
+    double *produtos;
+    int n,i;
+    printf("Informe o numero de produtos:");
+    scanf("%d",&n);
+    // é necessário usar o comando malloc para alocar a memória
+    produtos = (double *)malloc(n*sizeof(double));
+    for (i = 0; i < n; i++){
+    printf("Informe o valor do produto %d R$:", i+1);
     scanf("%lf", &produtos[i]);
-  } 
-
-  printf("\nProdutos cadastrados\n" );
-  for   (i = 0; i < n; i++){
-    printf("Produto %d  - R$: %.2f\n" ,i+1, produtos[i]);
-  }
-  
-  // ao terminar de usar o vetor, devemos liberar a memória
-  free(produtos);
-
-  return 0;
 }
----------------------------------------------------------------
-
-Suas considerações:
-
-Quais foram os valores de n testados?
-O que significa o valor de n?
-o que significa o valor de de sizeof(double) no 
-commando malloc(n*sizeof(double)).
-
-*/
+    printf("\nProdutos cadastrados\n");
+    for (i = 0; i < n; i++){
+    printf("Produto %d - R$: %f\n" ,i+1, produtos[i]);
+}
+  // ao terminar de usar o vetor, devemos liberar a memória
+free(produtos);
+}
+//*A váriavel 'n' fica responsável por informar quantos espaços de memória deverão ser utilizados, mas como cada tipo (int, float, double, char) possui seu respectivo valor de bits, o sizeof ficará encarregado de armazenar o tamanho desse tipo. Logo a variável 'n' vai ser multiplicado pelo sizeof que transformar o 'n' em bits exatos declarado pelo seu tipo informado e, logo sera alocado pela função malloc.*//
