6226d91067d692cf91f8ab78f2c84936d62a5b8d
nayaraof <80076803+nayaraof@users.noreply.github.com>
2021-08-18 19:56:47 -0300

    exe07

--- Lab10 - Pointers/ex07/ex07.c
+++ Lab10 - Pointers/ex07/ex07.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-    printf("<< Pointers >>\n");
+int main()
+{
+  double var[10] = {1,2,3,4,5,6,7,8,9,0};
+  int i;
 
-    return 0;
+    printf ("Valores do vetor:\n");
+    printf ("\n");
+  for (i=0; i<10; i++){
+     printf ("%lf\n", *(var+i));
+  }
+return 0;
+}
+
+//Exercício 10:
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  int var[10] = {1,2,3,4,5,6,7,8,9,0};
+  int i;
+
+    printf ("Valores do vetor:\n");
+    printf ("\n");
+  for (i=9; i>=0; i--){
+     printf ("%d\n", *(var+i));
+  }
+return 0;
 }
