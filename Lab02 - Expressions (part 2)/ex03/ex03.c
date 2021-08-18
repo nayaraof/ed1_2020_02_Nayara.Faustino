011a604a40575d4a0ed9397858ae602f3a78408e
nayaraof <80076803+nayaraof@users.noreply.github.com>
2021-08-17 21:07:20 -0300

    exe03

--- Lab02 - Expressions (part 2)/ex03/ex03.c
+++ Lab02 - Expressions (part 2)/ex03/ex03.c
@@ -1,23 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+int main()
 
-int main() {
-    printf("<< Calculo da Media >>\n");
+{
+
+    char letra;
+    float n1,n2,n3,media;
+
+    printf("<<Calculo da Media >>\n");
+
+    printf("Digite o nome do aluno : ");
+    scanf("%c", &letra);
+
+    printf("Digite a nota da prova 1 : ");
+    scanf("%f", &n1);
+
+     printf("Digite a nota da prova 2 : ");
+    scanf("%f", &n2);
+
+    printf("Digite a nota da prova 3 : ");
+    scanf("%f", &n3);
+
+        media=((n1*1)+(n2*1)+(n3*2))/4;
+    printf("A nota media do aluno  %c eh :   %f  " , letra ,media);
+
+    return 0;
 }
-
-/*
-=> Faça um algoritmo que calcule a média ponderada das notas de 3 provas. 
-A primeira e a segunda prova têm peso 1 e a terceira tem peso 2. Antes de o usuário 
-entrar com as notas do aluno ele deve informar a letra inicial do nome do aluno. 
-Ao final, mostrar o a letra inicial do nome do aluno juntamente com a sua média.
-
--------------------------------------------------------------------------------
-Exemplo de saída:
-
-<< Calculo da Media >>
-Digite o nome do aluno: T
-Digite a nota da prova 1: 40
-Digite a nota da prova 2: 40
-Digite a nota da prova 3: 80
-A nota media do aluno C. eh 60
--------------------------------------------------------------------------------
-*/
