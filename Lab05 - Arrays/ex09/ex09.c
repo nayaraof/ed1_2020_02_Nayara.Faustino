5f10fe94b0cd5e090b363fc3befd0ee06e115fd2
nayaraof <80076803+nayaraof@users.noreply.github.com>
2021-08-17 23:07:01 -0300

    exe09

--- Lab05 - Arrays/ex09/ex09.c
+++ Lab05 - Arrays/ex09/ex09.c
@@ -1,35 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-    printf("<< Media de n alunos. Maximo 100 alunos >>\n");
+int main()
+{
+	int alunos, i, j;
+	float soma;
+	printf ("<<Media de n alunos. Maximo 100 alunos>>\n");
+	printf("Entre com o numero maximo de alunos: ");
 
-    return 0;
+	scanf("%d",&alunos);
+	int aluno[alunos];
+
+	if(alunos > 100)
+		printf("Erro! O numero maximo de alunos permitido eh 100\n");
+	else{
+		for (int i = 0; i < alunos; i++)
+		{
+			printf("Digite a nota do aluno %d: ", i+1);
+			scanf("%d",&aluno[i]);
+		}
+		printf("\n");
+		printf("Relatorio de notas\n");
+
+		for (int j = 0; j < alunos; j++)
+		{
+			printf("A nota do aluno %d eh: %d\n", j, aluno[j]);
+			soma = soma + aluno[j];
+		}
+		printf("A media da turma eh: %.1f\n",soma/alunos);
+	}
+     return 0;
 }
-
-/*
-Faça um programa que calcula a média das notas dos alunos de uma turma. Note 
-que uma turma pode ter no máximo 100 alunos (mostrar uma mensagem de erro 
-caso o número escolhido seja maior que 100). Peça para o usuário entrar com 
-o número de alunos e ao final mostre a nota de todos os alunos.
-
-Exemplo de Saída
-<< Media de n alunos. Maximo 100 alunos >>
-Entre com o numero de alunos: 4
-Digite a nota do aluno 1: 6
-Digite a nota do aluno 2: 7
-Digite a nota do aluno 3: 5
-Digite a nota do aluno 4: 4
-
-Relatorio de Notas
-Nota do aluno 0: 6
-Nota do aluno 1: 7
-Nota do aluno 2: 5
-Nota do aluno 3: 4
-Media da turma:  5.5
-
-------------------------------------
-Exemplo de Saída 2
-<< Media de n alunos. Maximo 100 alunos >>
-Entre com o numero de alunos: 400
-Erro! O numero maximo de alunos permitido eh 100.
-*/
