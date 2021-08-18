a65806554564b363f65ac888d712d2973902edf0
nayaraof <80076803+nayaraof@users.noreply.github.com>
2021-08-17 23:30:51 -0300

    exe14

--- Lab05 - Arrays/ex14/ex14.c
+++ Lab05 - Arrays/ex14/ex14.c
@@ -1,33 +1,56 @@
+#include <stdlib.h>
 #include <stdio.h>
 
-int main(){
-    printf("<< Universidade X >>\n");
+int main()
+{
+  int n;
+  int qntalunos = 0;
+  int nroaluno = 0;
+  char classe = ' ';
+  int vnroaluno[10000];
+  int vclasse[10000];
+  float vcra[10000];
+  int i=0;
 
-    return 0;
+  printf("\n Universidade X \n\n");
+
+  printf("\nQuantos alunos serao cadastrados:\n\n");
+  fflush(stdin);
+  scanf("%d", &qntalunos);
+
+  if (qntalunos>10000){
+
+    printf("Numero maximo de alunos = 10.000\n\n");
+
+  }
+
+  else{
+
+        for(i=0;i<qntalunos;i++){
+
+            printf ("\nEntre com o numero do aluno:\n");
+            fflush(stdin);
+            scanf("%d", &vnroaluno[i]);
+
+            printf ("\nEntre com a classe social do aluno %d:\n", vnroaluno[i]);
+            fflush(stdin);
+            scanf("%c", &vclasse[i]);
+
+            printf ("\nEntre com o CRA do aluno %d:\n", vnroaluno[i]);
+            fflush(stdin);
+            scanf("%f", &vcra[i]);
+
+        }
+
+  }
+
+  printf("\n\n-----Alunos Cadastrados ----\n\n");
+
+  for(i=0;i<qntalunos;i++){
+
+  printf("Numero:%d Classe Social:%c CRA:%.2f\n", vnroaluno[i], vclasse[i], vcra[i]);
+
+  }
+
+  return 0;
 }
-
-/*
-Faça um programa que armazene informações sobre os alunos de uma universidade. 
-O número máximo de alunos que o sistema pode cadastrar é 10000. Os dados a 
-serem armazenados são: número de matrícula (inteiro); 
-classe social (A, B, C, D ou E); CRA (real). Ao iniciar o programa, informe 
-quantos usuários serão cadastrados. Depois de realizado o cadastro, mostre 
-todos os dados lidos. A saída do programa deve ser como o exemplo abaixo.
-* Proibido o uso de structs
-
-Exemplo de saída:
-<< Universidade X >>
-Quantos alunos serão cadastrados: 2
-
-Entre com o número do aluno: 5
-Entre com a classe social do aluno 5: B
-Entre com o CRA do aluno 5: 70
-
-Entre com o número do aluno: 7
-Entre com a classe social do aluno 7: B
-Entre com o CRA do aluno 7: 60
-
-==== Alunos Cadastrados ====
-Número: 5 Classe Social: B CRA: 70.00
-Número: 7 Classe Social: B CRA: 60.00
-*/
