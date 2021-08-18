6f3aa0161be8fd097978c9165f5b4830fcd3850d
nayaraof <80076803+nayaraof@users.noreply.github.com>
2021-08-17 21:01:50 -0300

    exe02

--- Lab02 - Expressions (part 2)/ex02/ex02.c
+++ Lab02 - Expressions (part 2)/ex02/ex02.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    printf("<< Calculo da Media >>\n");
+int main()
+{
+  printf ( " << Calculo da Media >> " );
+  printf("\n");
+
+  float n1,n2,media;
+  char letra;
+
+  printf("Digite o nome do aluno: " );
+   scanf(" %c",&letra);
+  printf("Digite a nota da prova 1: ");
+  scanf("%f", &n1);
+  printf("Digite a nota da prova 2: ");
+  scanf("%f", &n2);
+    printf("\n");
+
+    media = ((n1+n2)/2);
+
+    printf("A nota media do aluno %c  e:  %.2f:",letra, media);
+    return 0;
 }
-
-/*
-=> Altere o algoritmo anterior para que, antes de o usuário entrar com as 
-notas do aluno ele entre com a inicial do nome do aluno. Ao final, 
-mostrar a inicial do nome do aluno juntamente com a sua média.
-
--------------------------------------------------------------------------------
-Exemplo de saída:
-
-<< Calculo da Media >>
-Digite o nome do aluno: C
-Digite a nota da prova 1: 70
-Digite a nota da prova 2: 85
-A nota media do aluno C. eh 77.5
--------------------------------------------------------------------------------
-*/
