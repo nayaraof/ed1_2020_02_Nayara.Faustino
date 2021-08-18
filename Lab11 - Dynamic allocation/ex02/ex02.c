660459f2ac73199c25941341f7125777335f635a
nayaraof <80076803+nayaraof@users.noreply.github.com>
2021-08-18 20:01:58 -0300

    exe02

--- Lab11 - Dynamic allocation/ex02/ex02.c
+++ Lab11 - Dynamic allocation/ex02/ex02.c
@@ -1,29 +1,17 @@
 #include <stdio.h>
-//lembre-se de incluir as bibliotecas adequadas
+#include <stdlib.h>
 
-int main() {
-    printf("<< Guarda inteiros >>\n");
-    // Insira o seu código aqui.
+int main()
+{
+    int *produtos, i,n=5;
+    produtos = (int *)malloc (n*sizeof(int));
+
+    printf("Entre com os valores: ");
+    for (i = 0; i < n; i++)
+    scanf ("%d", &produtos[i]);
+
+    for (i = 0; i < n; i++)
+    printf("%d,", produtos[i]);
+
+    free(produtos);
 }
-
-/*
-2) Crie um programa que:
-    (a) Aloque dinamicamente um array de 5 números inteiros, 
-    (b) Peça para o usuário digitar os 5 números no espaço alocado, 
-    (c) Mostre na tela os 5 números, 
-    (d) Libere a memoria alocada.
-
-Exemplo de saída:
-<< Guarda inteiros >>
-Digite o numero(1): 4
-Digite o numero(2): 6
-Digite o numero(3): 8
-Digite o numero(4): 3
-Digite o numero(5): 9 
-
-O numero(1) eh 4
-O numero(2) eh 6
-O numero(3) eh 8
-O numero(4) eh 3
-O numero(5) eh 9
-*/
