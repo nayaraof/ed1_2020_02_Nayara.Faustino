c0bcb62715e7f783845a62e75fdb9f3f7ec6fc31
nayaraof <80076803+nayaraof@users.noreply.github.com>
2021-08-17 21:08:13 -0300

    exe04

--- Lab02 - Expressions (part 2)/ex04/ex04.c
+++ Lab02 - Expressions (part 2)/ex04/ex04.c
@@ -1,26 +1,41 @@
 #include <stdio.h>
 
-int main() {
-    printf("<< Calculo da Media >>\n");
+exe_04
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+
+{
+
+    char letra;
+    float n1,n2,n3,p1,p2,p3,media;
+
+    printf("<<Calculo da Media >>\n");
+
+    printf("Digite o nome do aluno : ");
+    scanf("%c", &letra);
+
+    printf("Digite a nota da prova 1 : ");
+    scanf("%f", &n1);
+
+     printf("Digite a nota da prova 2 : ");
+    scanf("%f", &n2);
+
+    printf("Digite a nota da prova 3 : ");
+    scanf("%f", &n3);
+
+    printf("Digite o peso da prova 1 : ");
+    scanf("%f", &p1);
+
+     printf("Digite o peso da prova 2 : ");
+    scanf("%f", &p2);
+
+     printf("Digite o peso da prova 3 : ");
+    scanf("%f", &p3);
+
+        media=((n1*p1)+(n2*p1)+(n3*p2))/5;
+    printf("A nota media do aluno  %c eh :   %f  " , letra ,media);
+
+    return 0;
 }
-
-/*
-=> Faça um algoritmo que calcule a média ponderada das notas de 3 provas. 
-A primeira e a segunda prova têm peso 1 e a terceira tem peso 2. Antes de o usuário 
-entrar com as notas do aluno ele deve informar a letra inicial do nome do aluno. 
-Ao final, mostrar o a letra inicial do nome do aluno juntamente com a sua média.
-
--------------------------------------------------------------------------------
-Exemplo de saída:
-
-<< Calculo da Media >>
-Digite o nome do aluno: M
-Digite a nota da prova 1: 70
-Digite a nota da prova 2: 50
-Digite a nota da prova 3: 65
-Digite o peso da prova 1: 1
-Digite o peso da prova 2: 1
-Digite o peso da prova 3: 2
-A nota media do aluno M. eh 60.83
--------------------------------------------------------------------------------
-*/
