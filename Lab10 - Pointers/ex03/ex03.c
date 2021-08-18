9d3f8314091585705775104a80b55325c26324bb
nayaraof <80076803+nayaraof@users.noreply.github.com>
2021-08-18 19:49:38 -0300

    exe03

--- Lab10 - Pointers/ex03/ex03.c
+++ Lab10 - Pointers/ex03/ex03.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-    printf("<< Pointers >>\n");
-    int  v1 = 1;
+int main()
+{
+    int v1 = 1;
     double v2 = 2;
     char c = 'a';
 
@@ -14,38 +15,24 @@ int main(){
     p_v2 = &v2;
     p_c = &c;
 
+    printf("\nEndereco de v1 %u", p_v1);
+    printf("\nEndereco de v2 %u", p_v2);
+    printf("\nEndereco de c %u", p_c);
+    printf("\nEndereco+1 de v1 %u", p_v1+1);
+    printf("\nEndereco+1 de v2 %u", p_v2+1);
+    printf("\nEndereco+1 de c %u", p_c+1);
+    printf("\nEndereco-1 de v1 %u", p_v1-1);
+    printf("\nEndereco-1 de v2 %u", p_v2-1);
+    printf("\nEndereco-1 de c %u", p_c-1);
+    printf("\nEndereco-2 de v1 %u", p_v1-2);
+    printf("\nEndereco-2 de v2 %u", p_v2-2);
+    printf("\nEndereco-2 de c %u", p_c-2);
+    printf("\nEndereco+2 de v1 %u", p_v1+2);
+    printf("\nEndereco+2 de v2 %u", p_v2+2);
+    printf("\nEndereco+2 de c %u", p_c+2);
 
-    printf("\nEndereço de v1 %u", p_v1);
-    printf("\nEndereço de v2 %u", p_v2);
-    printf("\nEndereço de c %u", p_c);
-
-    printf("\nEndereço+1 de v1 %u", p_v1+1);
-    printf("\nEndereço+1 de v2 %u", p_v2+1);
-    printf("\nEndereço+1 de c %u", p_c+1);
-
-    printf("\nEndereço-1 de v1 %u", p_v1-1);
-    printf("\nEndereço-1 de v2 %u", p_v2-1);
-    printf("\nEndereço-1 de c %u", p_c-1);
-
-    return 0;
+    //Nome:  var-2     var-1      var      var+1     var+2
+    //p_v1: 6356712 | 6356716 | 6356720 | 6356724 | 6356728
+    //p_v2: 6356696 | 6356704 | 6356712 | 6356720 | 6356728
+    //p_c : 6356609 | 6356710 | 6356711 | 6356712 | 6356713
 }
-
-/* 5)	Os operadores + e – funcionam com ponteiros. Chamamos de aritmética de ponteiros. 
-Considere o código abaixo. Complete o código e preencha a tabela abaixo com os endereços de cada printf.
-
-*** modificar o especificador de formato no printf caso seja necessário
-
-+-----------------------------+-------------------+-------------------+-----------------+-------------------+-------------------+
-| Nome variável               |                   |                   |                 |                   |                   |
-| / Endereços                 | Endereço var-2    | Endereço var-1    | Endereço var    | Endereço var+1    | Endereço var+2    |
-+-----------------------------+-------------------+-------------------+-----------------+-------------------+-------------------+
-|                             |                   |                   |                 |                   |                   |
-| p_v1                        |                   |                   |                 |                   |                   |
-+-----------------------------+-------------------+-------------------+-----------------+-------------------+-------------------+
-|                             |                   |                   |                 |                   |                   |
-| p_v2                        |                   |                   |                 |                   |                   |
-+-----------------------------+-------------------+-------------------+-----------------+-------------------+-------------------+
-|                             |                   |                   |                 |                   |                   |
-| p_c                         |                   |                   |                 |                   |                   |
-+-----------------------------+-------------------+-------------------+-----------------+-------------------+-------------------+
-*/
