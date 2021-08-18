106d5fb399df6918a2f542be2219e626403ba35b
nayaraof <80076803+nayaraof@users.noreply.github.com>
2021-08-18 19:50:48 -0300

    exe04

--- Lab10 - Pointers/ex04/ex04.c
+++ Lab10 - Pointers/ex04/ex04.c
@@ -1,7 +1,23 @@
+ 
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-    printf("<< Pointers >>\n");
+int main()
+{
+   int val[5] = {2,4,5,8,10};
+   printf ("Vetor atual:\n");
+   printf("{%d,%d,%d,%d,%d} ", val[0], val[1], val[2], val[3], val[4]);
 
+   int *v;
+   v = &val[2];
+   *v = 6;
+
+    printf ("\n");
+    printf ("\nVetor alterado:\n");
+    printf("{%d,%d,%d,%d,%d} ", val[0], val[1], val[2], val[3], val[4]);
+    printf ("\n");
+    printf ("\n<<Mapa de memoria>>");
+    printf ("\n%u", v);
     return 0;
+
 }
