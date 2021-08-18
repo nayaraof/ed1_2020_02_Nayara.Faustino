9d53c28bc100f0ae58318ff7c57a397f5fcb2bd2
nayaraof <80076803+nayaraof@users.noreply.github.com>
2021-08-17 21:01:00 -0300

    exe01

--- Lab02 - Expressions (part 2)/ex01/ex01.c
+++ Lab02 - Expressions (part 2)/ex01/ex01.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-    printf("<< Calculo da Media >>\n");
+int main()
+{
+  printf ( " << Calculo da Media >> " );
+  printf("\n");
+
+  float n1,n2,media;
+
+  printf("Digite a nota da prova 1: ");
+  scanf("%f", &n1);
+
+  printf("Digite a nota da prova 2: ");
+  scanf("%f", &n2);
+
+    media = ((n1+n2)/2);
+
+    printf("A nota media eh %.2f:", media);
+    return 0;
 }
-
-/*
-=> Faça um algoritmo que calcula a média das notas de 2 provas.
-
--------------------------------------------------------------------------------
-Exemplo de saída:
-
-<< Calculo da Media >>
-Digite a nota da prova 1: 80
-Digite a nota da prova 2: 90
-A nota media eh 85
--------------------------------------------------------------------------------
-*/
