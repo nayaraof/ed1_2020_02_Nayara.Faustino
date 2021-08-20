7dcb4c698c39fd4683c5c23653a34f561b6f562b
nayaraof <80076803+nayaraof@users.noreply.github.com>
2021-08-19 22:21:41 -0300

    Update ex02.c
    Exercicio 02 prova 19/08/2021

--- provas/p1/ex02/ex02.c
+++ provas/p1/ex02/ex02.c
@@ -2,8 +2,26 @@
 #include <stdlib.h>
 
 // Definir as estruturas necessárias aqui (copiar do exercício anterior):
-
+struct livro{
+    char *nome;
+    double preco;
+    int novo;          //1 para novo, 0 para usado
+    int ano;
+ };
+ typedef struct livro Livro;
 // Função:
+Livro buscar(Livro *lib, int size){
+    Livro aux;
+    aux.ano=10000000;
+    for(int i;i<size;i++){
+        if(lib[i].novo==0){//se não ,não novo,
+            if(lib[i].ano<aux.ano){
+                aux=lib[i];
+            }
+        }
+    } 
+    return aux;
+}
 // Essa função retorna o livro usado mais antigo
 // - Entrada: todos os livros cadastrados 
 // - Saída: índice do vetor indicando o livro usado mais antigo
@@ -12,11 +30,40 @@
 
 int main(){
 
+    int i, n;
+    Livro *lib, li;
+
    // Ex02: utilizando a função de buscar, mostre
    // todas as informações do livro *usado* mais antigo
 
    // Todas as funções de entrada e saída devem estar no programa
    // principal
+   printf("Informe quantos livros deseja cadastrar: ");
+
+    scanf("%d", &n);
+    lib = malloc(n*sizeof(Livro));
+    for ( i = 0; i < n; i++){
+
+         printf("Digite o nome do livro: ");
+        scanf("%s", &lib[i].nome);
+
+ 
+
+        printf("Digite o preco do livro: ");
+        scanf("%f", &lib[i].preco);
+
+ 
+
+        printf("Digite o status do livro: ");
+        scanf("%d", &lib[i].novo);
+
+ 
+
+        printf("Digite o ano do livro: ");
+        scanf("%d", &lib[i].ano);
+    }
+
+    li=buscar(lib,sizeof(lib));
 
    // Dica: para testar, crie um vetor estático preenchido com alguns valores (opcional)
    printf("Livro usado mais antigo: \n");
