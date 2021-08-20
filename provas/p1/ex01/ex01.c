0397757322ce5a30bbeb229cfcdbe9adc7c0b038
nayaraof <80076803+nayaraof@users.noreply.github.com>
2021-08-19 22:12:23 -0300

    Update ex01.c
    Exercicio 01 da primeira prova _19/08/2021

--- provas/p1/ex01/ex01.c
+++ provas/p1/ex01/ex01.c
@@ -9,7 +9,13 @@
 
 
 // Definir as estruturas necessárias aqui:
-
+struct livro{
+    char nome[100];
+    double preco;
+   int  novo;  //1 para novo, 0 para usado
+    int ano;
+ };
+ typedef struct livro Livro;
 
 int main(){
 
@@ -17,6 +23,48 @@ int main(){
     //  - ler do teclado a quantidade de livros que o usuário escolheu
     //  - ler do teclado as informações dos livros
     printf("Informe quantos livros deseja cadastrar: ");
-  
+    int main()
+ {
+    int c;
+
+ 
+
+    printf("Informe quantos livros deseja cadastrar: ");
+    scanf("%d", &c);
+
+ 
+
+    Livro *livro = malloc(sizeof(Livro)*c);
+
+ 
+
+    for (int x = 0; x < c; x++){
+        printf("Digite o nome do livro: ");
+        scanf("%s", &livro[x].nome);
+
+ 
+
+        printf("Digite o preco do livro: ");
+        scanf("%f", &livro[x].preco);
+
+ 
+
+        printf("Digite o status do livro: ");
+        scanf("%d", &livro[x].novo);
+
+ 
+
+        printf("Digite o ano do livro: ");
+        scanf("%d", &livro[x].ano);
+    }
+
+ 
+
+    printf(livro[0].nome);
+    printf(livro[1].nome);
+ }
+    
+    
+  free(livro);
     return 0;
 }
