b9b32b7638df1c72c045cb3998b598c95f9ea1a0
nayaraof <80076803+nayaraof@users.noreply.github.com>
2021-10-07 22:40:34 -0300

    Update ex02.c
    concat_3

--- provas/p3/ex02.c
+++ provas/p3/ex02.c
@@ -1,30 +1,54 @@
-/*
-Considere o TAD TDLinkedList que serve para manipular uma lista duplamente
-encadeada de alunos (a lista usada na prática 16 – copiar o código da 
-SUA PRÁTICA)
 
-Implemente uma função chamada concat3, cuja funcionalidade é concatenar 
-(juntar) 3 listas. Seu cabeçalho é o seguinte:
 
-int list_concat3(TDLinkedList *pre, TDLinkedList *in, TDLinkedList *pos)
+int  concatena(Lista * l1, Lista * l2,Lista * l3){
+    Lista aux,aux2;
 
-pre  - lista a ser concatenada e que vai ficar no início (antes de /in/). 
-      É também a lista de destino final (lista que receberá a concatenação 
-      de todas as listas).  
-in – lista a ser concatenada e que vai ficar entre as listas /pre/ e /pos/. 
-pos – lista a ser concatenada no final (depois de /in/) 
 
-Retorno da função: 0 para sucesso; -1 para qualquer tipo de erro
-Na implementação do list_concat3, *nenhum elemento é copiado ou movido*,
-somente os ponteiros internos das três listas são rearranjados.
-Após a concatenação, as listas continuarão existindo, no entanto,
-as listas in e pos ficarão sem elementos (vazia). 
-O resultado final da concatenação ficará na lista /pre/.  
+    aux2 = *l3;
 
-Saída concatenada (armazenada em \pre\):
-pre <-> in <-> pos
-*/
+    aux = *l1;
 
-int list_concat3(TDLinkedList *pre, TDLinkedList *in, TDLinkedList *pos){
+    while(aux != NULL){
+        Lista n = (Lista) malloc(sizeof(struct no));
 
+        if(n == NULL){
+            return 0;
+        }
+
+
+        n->info = aux->info;
+
+
+        if(aux2 == NULL){
+            aux2 = n;
+        }else{
+            aux2->prox = n;
+            aux2 = aux2->prox;
+        }
+
+        aux = aux->prox;
+    }
+
+   aux = *l2;
+
+    while(aux != NULL){
+        Lista n = (Lista) malloc(sizeof(struct no));
+        if(n == NULL)
+            return 0;
+        n->info = aux->info;
+        if(aux2 == NULL){
+            aux2 = n;
+        }else{
+            aux2->prox = n;
+            aux2 = aux2->prox;
+        }
+
+        aux = aux->prox;
+    }
+
+    if(aux2 != NULL)
+        aux2->prox = NULL;
+
+    return 1;
 }
+  }
