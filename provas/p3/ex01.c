3759849a10c26894392cf55ce5e05d859fc90cf7
nayaraof <80076803+nayaraof@users.noreply.github.com>
2021-10-07 22:41:37 -0300

    Update ex01.c
    erase_max

--- provas/p3/ex01.c
+++ provas/p3/ex01.c
@@ -12,6 +12,23 @@ lista (que estão no .h)
 
 */
 
-int list_erase_max_nota_n1(TDLinkedList *li){
+  
+  int list_erase_max_nota_n1(TDLinkedList * li){
+
+    if(list_size(li)==NULL)
+        return 0;
+    aluno aux = li->begin;
+    aluno aux = 
+    float max = 10;//total
+    i=0;
+    while (aux->prox != NULL){
+        if(aux->dados->n1=max){
+            max=aux->dados->n1
+            list_erase(aux,i);
+            
+        }
+        aux=aux->prox;
+        i++;
+    }
 
 }
