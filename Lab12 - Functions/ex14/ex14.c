bb90cfe2f9c4565bc013e7b5d468833fb219be0f
nayaraof <80076803+nayaraof@users.noreply.github.com>
2021-10-25 10:17:36 -0300

    Update ex14.c

--- Lab12 - Functions/ex14/ex14.c
+++ Lab12 - Functions/ex14/ex14.c
@@ -1,23 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-    printf("Digite o ponto: ");
-    printf("Digite a constante: ");
+struct ponto {
+	float x;
+	float y;
+};
+typedef struct ponto ponto;
 
-    return 0;
+void imprime_ponto(ponto p1);
+void multponto(ponto * p1, float * con);
+
+int main() {
+
+	ponto p1;
+
+	float con;
+
+    multponto(&p1,&con);
+    imprime_ponto(p1);
+
+	return 0;
 }
 
-/*
-=> Faça um procedimento que multiplique o valor de um ponto 
-por uma constante e altere o valor do ponto. Deve ser usada 
-passagem por referência e retorno void.
+void multponto(ponto * p1, float * con){
 
+    printf("Digite o valor dos pontos");
+    scanf("%f %f",&(*p1).x,&(*p1).y);
 
--------------------------------------------------------------------------------
-Exemplo de Saída:
+    float a;
+    printf("Digite o valor da constante");
 
-Digite o ponto: 1,2
-Digite a constante: 5
-Resultado: (1,2) * 5 = (5,10)
--------------------------------------------------------------------------------
-*/
+    scanf("%f",&a);
+
+    *con = a;
+
+    (*p1).x = (*p1).x * a;
+    (*p1).y = (*p1).y * a;
+}
+void imprime_ponto(ponto p1) {
+	printf("O valor de ponto eh(%.2f, %.2f)", p1.x,p1.y);
+}
+
