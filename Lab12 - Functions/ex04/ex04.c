0fc25f54848c993766ac1405239240023c95272e
nayaraof <80076803+nayaraof@users.noreply.github.com>
2021-10-25 09:59:50 -0300

    Update ex04.c

--- Lab12 - Functions/ex04/ex04.c
+++ Lab12 - Functions/ex04/ex04.c
@@ -1,27 +1,71 @@
-#include <stdio.h>
+4)
 
-int main(){
-    printf("Digite o valor de x: ");
-    printf("Digite o valor de n: ");
+#include<stdio.h>
 
-    return 0;
+
+int main()
+{
+
+float x = 0.0;
+int n = 0;
+
+void CalculaX(float x, int n){
+    int i=0;
+     float guardax = 0.0;
+     float guardafrac = 0;
+
+     printf("Digite valor de x");
+     fflush(stdin);
+     scanf("%f",&x);
+
+     printf("Digite o valor de n");
+     fflush(stdin);
+     scanf("%d",&n);
+
+
+     if(n>0){
+
+        guardax = x;
+
+        for(i=1;i<n;i++){
+
+            x = x * guardax;
+            printf("%f",guardax);
+        }
+
+        printf("%f", x);
+
+        }else if (n==0){
+
+             x = 1;
+
+             printf("%f",x);
+
+
+
+        } else {
+
+            n = abs(n);
+            guardafrac = 1/x;
+            float z=0.0;
+            
+
+            z=1/x;
+
+            for(i=2;i<=n;i++){
+
+            x = z * guardafrac;
+            z=x;
+
+            }
+
+            printf("x: %f", x);
+        }
+
+     }
+
+    CalculaX(x,n);
+
+  return 0;
+
 }
-
-/*
-=> Faça uma função para calcular xn. Não utilizar o operador pow (nem ^ - que não existe em C).
-No programa principal permita ao usuário informar o valor de x e de n (inteiro). 
-Lembre que n pode assumir valor negativo.
-
-Ex: Elevado(2,4) tem como saída 16; 
-    
-
-No programa principal, solicite ao usuário o valor de x e o valor de n.
-
--------------------------------------------------------------------------------
-Exemplo de Saída:
-
-Digite o valor de x: 2
-Digite o valor de n: 4
-O valor de 2 elevado a 4 eh 16
--------------------------------------------------------------------------------
-*/
