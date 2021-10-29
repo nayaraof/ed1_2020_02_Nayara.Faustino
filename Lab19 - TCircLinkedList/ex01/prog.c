d109207bc2330bf29cdb3e82443392bd74f2db5c
nayaraof <80076803+nayaraof@users.noreply.github.com>
2021-10-28 21:06:49 -0300

    TCircLinkedList.h

--- Lab19 - TCircLinkedList/ex01/prog.c
+++ Lab19 - TCircLinkedList/ex01/prog.c
@@ -1,7 +1,27 @@
-#include <stdio.h>
+#define SUCCESS 0
+#define OUT_OF_MEMORY -1
+#define ELEM_NOT_FOUND -2
+#define INVALID_NULL_POINTER -3
+#define INVALID_RANGE -4
 
-int main()
-{
+struct aluno{
+ int matricula;
+ char nome[30];
+ float n1,n2,n3;
+};
 
-    return 0;
-}
+typedef struct circlist List;
+typedef struct aluno aluno;
+
+List* list_create();
+int list_free(List *li);
+int list_size(List *li);
+int list_push_front(List *li, struct aluno al);
+int list_front(List *li, struct aluno *al);
+int list_push_back(List *li, struct aluno al);
+int list_pop_front(List *li);
+int list_back(List *li, struct aluno *al);
+int list_pop_back(List *li);
+int list_print_forward(List *li);
+int list_erase(List *li, int pos);
+int list_find_pos(List *li, int pos, struct aluno *al);
